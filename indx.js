d612301dcd00426e554e40856e35d87e476efb3b
Varkees <125126697+Varkees@users.noreply.github.com>
2024-07-16 20:10:25 +0530

    Update indx.js

--- indx.js
+++ indx.js
@@ -2,8 +2,5 @@ var jack=[1,2,3,4,5];
 console.log(jack);
 var jack1=[1,2,3,4];
 console.log(jack1);
-<<<<<<< HEAD
-jkgit
-=======
-Edit for pull request
->>>>>>> c444d018f83d1ee49bcff67c412080e56b209ea4
+
+//got to ceat onother file
